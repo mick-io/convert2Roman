885e3325b4c4a24bb1d27033c2703eca2db85df4
Michael Piereder <I21414@xactware.com>
2017-11-17 20:09:37 -0700

    removed default export and added new input sanitization

--- index.js
+++ index.js
@@ -3,43 +3,55 @@
 /**
  * @param input: number - Value to be converted to a Roman Numeral
  */
-exports.default = function (input) {
+function convert2Roman(input) {
 
-  /* Sanitizing Input. */
-  input = Math.round(Math.abs(input));
-
-  /* Static Loop Variables */
-  var romanNumerals = {
-    M: 1000,
-    CM: 900,
-    D: 500,
-    C: 100,
-    XC: 90,
-    L: 50,
-    XL: 40,
-    X: 10,
-    IX: 9,
-    V: 5,
-    IV: 4,
-    I: 1,
-  };
-  var romanCharacters = Object.keys(romanNumerals);
-
-  /* Dynamic Loop Variables */
-  var char = "";
-  var num = 0;
-
-  /* Return Variable */
-  var output = "";
-
-  for (var i = 0; i <= romanCharacters.length; i++) {
-    char = romanCharacters[i];
-    num = romanNumerals[romanCharacters[i]];
-    while (input - num >= 0) {
-      output += char;
-      input -= num;
+    /* Sanitizing Input. */
+    if (typeof input !== "number") {
+        input = parseInt(input);
     }
-  }
 
-  return output;
+    if (input == 0) {
+        return "";
+    } else if (!input) {
+        throw new Error("convert2Roman must be called with typeof 'number'");
+    }
+
+    input = Math.round(Math.abs(input));
+
+    /* Static Loop Variables */
+    var romanNumerals = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1,
+    };
+    var romanCharacters = Object.keys(romanNumerals);
+
+    /* Dynamic Loop Variables */
+    var char = "";
+    var num = 0;
+
+    /* Return Variable */
+    var output = "";
+
+    for (var i = 0; i <= romanCharacters.length; i++) {
+        char = romanCharacters[i];
+        num = romanNumerals[romanCharacters[i]];
+        while (input - num >= 0) {
+            output += char;
+            input -= num;
+        }
+    }
+
+    return output;
 };
+
+module.exports = convert2Roman;
