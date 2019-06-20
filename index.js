4e50fb7611e515390b3efc63c41568abb641d3e2
mick-io <mick.io@pm.me>
2019-06-20 09:55:40 -0600

    minor changes

--- index.js
+++ index.js
@@ -5,46 +5,39 @@
  * @param {number} input the value to be converted to a Roman Numeral
  * @returns {string} The input value in roman numerals.
  */
-module.exports = function (input) {
-    var EMPTY_STRING = "";
-    var ZERO = 0;
-
-    if (typeof input !== "number") {
-        throw new Error("convert2Roman must be called with typeof 'number'");
+module.exports = function(input) {
+  if (typeof input !== "number") {
+    throw new Error("convert2Roman must be called with typeof 'number'");
+  }
+  if (input === ZERO) {
+    return "";
+  }
+  input = Math.round(Math.abs(input));
+  var conversions = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
+  };
+  var romanNums = Object.keys(conversions);
+  var num = 0;
+  var char = "";
+  var output = "";
+  for (var i = 0; i <= romanNums.length; i++) {
+    char = romanNums[i];
+    num = conversions[romanNums[i]];
+    while (input - num >= 0) {
+      output += char;
+      input -= num;
     }
-
-    if (input === ZERO) {
-        return EMPTY_STRING;
-    }
-
-    input = Math.round(Math.abs(input));
-
-    var romanNumerals = {
-        M: 1000,
-        CM: 900,
-        D: 500,
-        C: 100,
-        XC: 90,
-        L: 50,
-        XL: 40,
-        X: 10,
-        IX: 9,
-        V: 5,
-        IV: 4,
-        I: 1,
-    };
-    var romanCharacters = Object.keys(romanNumerals);
-    var char = EMPTY_STRING;
-    var num = 0;
-    var output = EMPTY_STRING;
-
-    for (var i = ZERO; i <= romanCharacters.length; i++) {
-        char = romanCharacters[i];
-        num = romanNumerals[romanCharacters[i]];
-        while (input - num >= ZERO) {
-            output += char;
-            input -= num;
-        }
-    }
-    return output;
+  }
+  return output;
 };
